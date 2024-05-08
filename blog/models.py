c931746fe16f6c5195f80c0880aa88b0cc7418cd
Rebaz Pappiland <rebazp@gmail.com>
2024-05-08 09:12:22 +0000

    setup comment model

--- blog/models.py
+++ blog/models.py
@@ -28,3 +28,16 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=70)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
