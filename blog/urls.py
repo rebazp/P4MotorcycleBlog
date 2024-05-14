919c833b2910a96ddac0340c64538f4d9bb47ace
Rebaz Pappiland <rebazp@gmail.com>
2024-05-14 13:53:43 +0000

    change url order

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,11 @@ from .views import PostListView, AddPostView, UpdatePostView, PostDetail, Delete
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('post_list/', PostListView.as_view(), name='post_list'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+
     path('article/edit/<int:pk>/', UpdatePostView.as_view(), name='update_post'),
     path('article/edit/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
     path('article/edit/<int:pk>/update', UpdateCommentView.as_view(), name='update_comment'),
