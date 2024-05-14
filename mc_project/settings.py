11d0cbab6b1c316980909ccc168e068f2929a4c3
Rebaz Pappiland <rebazp@gmail.com>
2024-05-14 20:33:05 +0000

    fixed the footer color and links

--- mc_project/settings.py
+++ mc_project/settings.py
@@ -176,3 +176,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 900
+
+SESSION_SAVE_EVERY_REQUEST = True
