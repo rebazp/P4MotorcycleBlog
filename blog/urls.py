88c8a5b64a72a8ae9af90781014f227fb70022c0
Rebaz Pappiland <rebazp@gmail.com>
2024-05-16 13:18:41 +0000

    add comments urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import PostListView, AddPostView, UpdatePostView, PostDetail, DeletePostView, UpdateCommentView, DeleteCommentView, custom_handler404, custom_handler500, custom_login
 
 
+# Urls for the project
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
@@ -19,3 +20,11 @@ urlpatterns = [
 
 handler404 = custom_handler404
 handler500 = custom_handler500
+
+
+# The code in this file is inspired from:
+# My own previous projects and knowledge
+# Code Institute, I think therefore i blog project
+# Youtube series Django Tutorial by [Net Ninja](https://www.youtube.com/watch?v=n-FTlQ7Djqc&list=PL4cUxeGkcC9ib4HsrXEYpQnTOTZE1x0uc&index=1&ab_channel=NetNinja)
+# Youtube series Python Django Tutorial by [Corey Schafer](https://www.youtube.com/watch?v=UmljXZIypDc&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=1&ab_channel=CoreySchafer)
+# Yoube Python Django Web Framework by [FreeCodeCamp](https://www.youtube.com/watch?v=F5mRW0jo-U4&ab_channel=freeCodeCamp.org)
